f5a49da87c5d376edf0046cc6a9eacb724451b76
QinZelin <m18822306200@163.com>
2024-10-31 21:27:40 +0800

    add Object_Detection

---
+++ Object_Detection/configs/obb/oriented_rcnn/faster_rcnn_orpn_our_imp_biformer_diff_h_w_fpn_3x_hrsc.py
@@ -0,0 +1,161 @@
+_base_ = [
+    '../_base_/datasets/hrsc.py',
+    '../_base_/schedules/schedule_3x.py',
+    '../../_base_/default_runtime.py'
+]
+
+optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+lr_config = dict(step=[24, 33])
+
+#optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+# RetinaNet nms is slow in early stage, disable every epoch evaluation
+#evaluation = dict(metric='mAP')
+
+model = dict(
+    type='OrientedRCNN',
+    pretrained='pth/biformer_small_best.pth',
+    backbone=dict(
+        type='BiFormer',
+        depth=[4, 4, 18, 4],
+        embed_dim=[64, 128, 256, 512],
+        img_size=[800,800],
+        mlp_ratios=[3, 3, 3, 3],
+        # use_checkpoint_stages=[0, 1, 2, 3],
+        use_checkpoint_stages=[],
+        # ------------------------------
+        #n_win=7,
+        #training resolution is 512
+        #n_win=8,
+        #training resolution is 800
+        n_win=12,
+        kv_downsample_mode='identity',
+        kv_per_wins=[-1, -1, -1, -1],
+        topks=[1, 4, 16, -2],
+        side_dwconv=5,
+        before_attn_dwconv=3,
+        layer_scale_init_value=-1,
+        qk_dims=[64, 128, 256, 512],
+        head_dim=32,
+        param_routing=False, diff_routing=False, soft_routing=False,
+        pre_norm=True,
+        pe=None,
+        out_indices=(0, 1, 2, 3),
+        auto_pad=True,
+        # upernet requires a larger dpr
+        drop_path_rate=0.3
+    ),
+    neck=dict(
+        type='FPN',
+        in_channels=[64, 128, 256, 512],
+        out_channels=256,
+        num_outs=5),
+    rpn_head=dict(
+        type='OrientedRPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='MidpointOffsetCoder',
+            target_means=[.0, .0, .0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0, 0.5, 0.5]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)),
+    roi_head=dict(
+        type='OBBStandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='OBBSingleRoIExtractor',
+            roi_layer=dict(type='RoIAlignRotated', out_size=7, sample_num=2),
+            out_channels=256,
+            extend_factor=(1.4, 1.2),
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='OBBShared2FCBBoxHead',
+            start_bbox_type='obb',
+            end_bbox_type='obb',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='OBB2OBBDeltaXYWHTCoder',
+                target_means=[0., 0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2, 0.1]),
+            reg_class_agnostic=True,
+            loss_cls=dict(
+                type='CrossEntropyLoss',
+                use_sigmoid=False,
+                loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                           loss_weight=1.0))))
+# model training and testing settings
+train_cfg = dict(
+    rpn=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.7,
+            neg_iou_thr=0.3,
+            min_pos_iou=0.3,
+            match_low_quality=True,
+            gpu_assign_thr=200,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='RandomSampler',
+            num=256,
+            pos_fraction=0.5,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=False),
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False),
+    rpn_proposal=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=2000,
+        max_num=2000,
+        nms_thr=0.8,
+        min_bbox_size=0),
+    rcnn=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.5,
+            min_pos_iou=0.5,
+            match_low_quality=False,
+            ignore_iof_thr=-1,
+            iou_calculator=dict(type='OBBOverlaps')),
+        sampler=dict(
+            type='OBBRandomSampler',
+            num=512,
+            pos_fraction=0.25,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=True),
+        pos_weight=-1,
+        debug=False))
+test_cfg = dict(
+    rpn=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=2000,
+        max_num=2000,
+        nms_thr=0.8,
+        min_bbox_size=0),
+    rcnn=dict(
+        score_thr=0.05, nms=dict(type='obb_nms', iou_thr=0.1), max_per_img=2000))
+val_cfg = dict(
+    rpn=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=2000,
+        max_num=2000,
+        nms_thr=0.8,
+        min_bbox_size=0),
+    rcnn=dict(
+        score_thr=0.05, nms=dict(type='obb_nms', iou_thr=0.1), max_per_img=2000))
